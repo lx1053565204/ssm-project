372f71a0d24abccae9f79bf5ffbfc548e7f4d16c
luxu <2463219846@qq.com>
2020-02-11 16:32:28 +0800

    2

---
+++ src/main/java/com/luxu/ssm/SsmProjectApplication.java
@@ -0,0 +1,13 @@
+package com.luxu.ssm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsmProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsmProjectApplication.class, args);
+	}
+
+}
